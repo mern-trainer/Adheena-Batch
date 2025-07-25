784a7604b199d78a6a7a17c1d4d41a080775805c
VISHNU M K <mail.vishnumk@gmail.com>
2025-07-25 10:11:58 +0530

    react state

--- ReactJS/app/src/App.jsx
+++ ReactJS/app/src/App.jsx
@@ -1,11 +1,23 @@
-import Header from "./components/Header"
-import LandingPage from "./pages/LandingPage"
+import { useState } from "react"
+
+// states -> states is an object to handle the data in a component
+// hooks -> functions to handle the states and life cycle of a component
+
+// useState() -> hook to handle the states
+
+// 2 type of components -> function based and class based
 
 const App = () => {
-    return <div>
-        <Header />
-        <LandingPage />
-        HELLO
+
+    const [counter, setCounter] = useState(10);
+
+    const increment = () => {
+        setCounter(counter + 1)
+    }
+
+    return <div className="d-flex flex-column gap-2 align-items-center mt-5">
+        <div>{counter}</div>
+        <button className="btn btn-success" onClick={increment}>Increment</button>
     </div>
 }
 
---
+++ ReactJS/app/src/pages/Sample.jsx
@@ -0,0 +1,8 @@
+const Sample = () => {
+    return <div className="d-flex  justify-content-center align-items-center vh-100 text-primary fs-1 fw-bold">
+        <button className="btn btn-secondary">Sample</button>
+        
+    </div>
+}
+
+export default Sample
